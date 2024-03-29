38f7e1f07f7b381c00bf60dce95c2bd74db442f2
Axel <meister.axel@gmail.com>
2024-03-29 13:55:19 +0100

    Implemented Instruction component to give an overview of the application

---
+++ src/components/Instruction.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style.css";
+
+function Instruction() {
+  return (
+    <div>
+      <p>
+        This tool lets you set a weekly budget and track expenses, starting with
+        placeholder data. You can reset everything easily, input your budget,
+        and submit it. Enter expenses for each day or select days and submit
+        them to the tracker.
+      </p>
+    </div>
+  );
+}
+
+export default Instruction;
