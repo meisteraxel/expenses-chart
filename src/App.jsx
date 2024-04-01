b1f7866a2cd6bda7fa73879c9079c67bdd4e1f2e
Axel <meister.axel@gmail.com>
2024-04-01 13:47:14 +0200

    Budget and Expenses are now saved to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,51 @@ import Chart from "./components/Chart";
 import Input from "./components/Input";
 
 function App() {
-  const [budget, setBudget] = React.useState(1000);
-  const [expenses, setExpenses] = React.useState([
-    {
-      day: "mon",
-      amount: 20.45,
-    },
-    {
-      day: "tue",
-      amount: 32.23,
-    },
-    {
-      day: "wed",
-      amount: 42.65,
-    },
-    {
-      day: "thu",
-      amount: 65.78,
-    },
-    {
-      day: "fri",
-      amount: 23,
-    },
-    {
-      day: "sat",
-      amount: 11,
-    },
-    {
-      day: "sun",
-      amount: 5,
-    },
-  ]);
+  const [budget, setBudget] = React.useState(
+    JSON.parse(localStorage.getItem("budget")) || 1000
+  );
+  const [expenses, setExpenses] = React.useState(
+    JSON.parse(localStorage.getItem("expenses")) || [
+      {
+        day: "mon",
+        amount: 20.45,
+      },
+      {
+        day: "tue",
+        amount: 32.23,
+      },
+      {
+        day: "wed",
+        amount: 42.65,
+      },
+      {
+        day: "thu",
+        amount: 65.78,
+      },
+      {
+        day: "fri",
+        amount: 23,
+      },
+      {
+        day: "sat",
+        amount: 11,
+      },
+      {
+        day: "sun",
+        amount: 5,
+      },
+    ]
+  );
+
+  // Save Budget to local storage
+  React.useEffect(() => {
+    localStorage.setItem("budget", JSON.stringify(budget));
+  }, [budget]);
+
+  // Save expenses to local storage
+  React.useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
 
   function keyPress(event) {
     if (event.key === "Enter") {
